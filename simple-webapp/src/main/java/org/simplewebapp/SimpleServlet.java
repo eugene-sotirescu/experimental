665a0fd3525e82c721b374e2370b068dc372f3c6
e s <e@s.com>
2014-06-18 16:33:13 -0400

    updates

--- simple-webapp/src/main/java/org/simplewebapp/SimpleServlet.java
+++ simple-webapp/src/main/java/org/simplewebapp/SimpleServlet.java
@@ -1,4 +1,4 @@
-package org.sonatype.mavenbook.web;
+package org.simplewebapp;
 
 import java.io.*;
 import java.util.Date;
@@ -7,11 +7,22 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class SimpleServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		response.setContentType("text/html");
 		Date now = new Date();
 		PrintWriter out = response.getWriter();
+		out.println("<pre>");
 		out.println("The SimpleServlet was Executed at " + now);
+		out.println("output of getContextPath: " + request.getContextPath());
+		out.println("output of getRequestURL: " + request.getRequestURL());
+		out.println("output of getServletPath: " + request.getServletPath());
+		out.println("</pre>");
 		out.flush();
 		out.close();
 	}
